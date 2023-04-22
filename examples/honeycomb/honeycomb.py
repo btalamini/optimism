8871823b08dc28f0d5068acdbf4eebcd91433f52
Brandon Talamini <talamini1@llnl.gov>
2023-04-21 21:26:35 -0700

    Implement contact

--- examples/honeycomb/honeycomb.py
+++ examples/honeycomb/honeycomb.py
@@ -1,7 +1,11 @@
+from functools import partial
 import jax
 import jax.numpy as np
 import numpy as onp
 
+from optimism import AlSolver
+from optimism.contact import Contact
+from optimism import ConstrainedObjective
 from optimism import EquationSolver
 from optimism import FunctionSpace
 from optimism.material import Neohookean
@@ -13,8 +17,8 @@ from optimism import ReadExodusMesh
 from optimism import SparseMatrixAssembler
 from optimism import VTKWriter
 
-mesh = ReadExodusMesh.read_exodus_mesh("honeycomb.g")
-mesh = Mesh.create_higher_order_mesh_from_simplex_mesh(mesh, order=2, useBubbleElement=False, copyNodeSets=False, createNodeSetsFromSideSets=True)
+mesh = ReadExodusMesh.read_exodus_mesh("honeycomb_tri3.g")
+#mesh = Mesh.create_higher_order_mesh_from_simplex_mesh(mesh, order=2, useBubbleElement=False, copyNodeSets=False, createNodeSetsFromSideSets=True)
 
 ebcs = [FunctionSpace.EssentialBC(nodeSet="left", component=0),
         FunctionSpace.EssentialBC(nodeSet="right", component=0),
@@ -38,7 +42,16 @@ solidMechanics = Mechanics.create_mechanics_functions(fs,
                                                       mode2D="plane strain",
                                                       materialModel=materialModel)
 
-solverSettings = EquationSolver.get_settings()
+contactQuadRule = QuadratureRule.create_quadrature_rule_1D(4)
+
+solverSettings = EquationSolver.get_settings(tol=1e-7)
+
+alSettings = AlSolver.get_settings(max_gmres_iters=300,
+                                   num_initial_low_order_iterations=5,
+                                   use_second_order_update=True,
+                                   penalty_scaling = 1.05,
+                                   target_constraint_decrease_factor=0.5,
+                                   tol=2e-7)
 
 outputForce = []
 outputDisp = []
@@ -53,7 +66,7 @@ def get_ubcs(p):
 def create_field(Uu, p):
     return ebcManager.create_field(Uu, get_ubcs(p))
 
-def energy_function(Uu, p):
+def compute_potential(Uu, p):
     U = create_field(Uu, p)
     internalVariables = p[1]
     return solidMechanics.compute_strain_energy(U, internalVariables)
@@ -65,6 +78,20 @@ def assemble_sparse_preconditioner(Uu, p):
     return SparseMatrixAssembler.assemble_sparse_stiffness_matrix(elementStiffnesses,
                                                                   fs.mesh.conns,
                                                                   ebcManager)
+
+h = 2.5e-4 # approximate element size
+closest_distance = partial(Contact.compute_closest_distance_to_each_side_smooth, smoothingTol=1e-1*h)
+
+def compute_constraints(Uu, p):
+    U = create_field(Uu, p)
+    interactionList1 = p[3][0]
+    contactDists1 = closest_distance(mesh, U, contactQuadRule, interactionList1, mesh.sideSets["cell_1_contact_B"])
+    interactionList2 = p[3][1]
+    contactDists2 = closest_distance(mesh, U, contactQuadRule, interactionList2, mesh.sideSets["cell_2_contact_B"])
+    interactionList3 = p[3][2]
+    contactDists3 = closest_distance(mesh, U, contactQuadRule, interactionList3, mesh.sideSets["cell_3_contact_B"])
+    return np.hstack((contactDists1.ravel(), contactDists2.ravel(), contactDists3.ravel()))
+
 
 def compute_energy_from_bcs(Uu, Ubc, internalVariables):
     U = ebcManager.create_field(Uu, Ubc)
@@ -107,29 +134,66 @@ def write_output(U, p, step):
                  displacement=np.array(outputDisp))
 
 
+def update_contact_params(Uu, p):
+    U = create_field(Uu, p)
+    maxContactNeighbors = 4
+    
+    sideA = mesh.sideSets["cell_1_contact_A"]
+    sideB = mesh.sideSets["cell_1_contact_B"]
+    interactionList1 = Contact.get_potential_interaction_list(sideA, sideB,
+                                                              mesh, U, maxContactNeighbors)
+    
+    sideA = mesh.sideSets["cell_2_contact_A"]
+    sideB = mesh.sideSets["cell_2_contact_B"]
+    interactionList2 = Contact.get_potential_interaction_list(sideA, sideB,
+                                                              mesh, U, maxContactNeighbors)
+    
+    sideA = mesh.sideSets["cell_3_contact_A"]
+    sideB = mesh.sideSets["cell_3_contact_B"]
+    interactionList3 = Contact.get_potential_interaction_list(sideA, sideB,
+                                                              mesh, U, maxContactNeighbors)
+    p = Objective.param_index_update(p, 3, (interactionList1, interactionList2, interactionList3))
+    return p
+
+
 def run():
     Uu = ebcManager.get_unknown_values(np.zeros_like(mesh.coords))
     disp = 0.0
     ivs = solidMechanics.compute_initial_state()
     p = Objective.Params(disp, ivs)
 
+    searchFrequency = 1
+    p = update_contact_params(Uu, p)
+
+    c = compute_constraints(Uu, p)
+    initialMultiplier = 4.0
+    kappa0 = initialMultiplier * np.ones_like(c)
+    lam0 = 1e-4*np.abs(kappa0*c)
+    print(f"c.shape={c.shape}")
+    print(f"kappa0.shape={kappa0.shape}")
+    print(f"lam0.shape={lam0.shape}")
+    
     precondStrategy = Objective.PrecondStrategy(assemble_sparse_preconditioner)
-    objective = Objective.Objective(energy_function, Uu, p, precondStrategy)
+    #objective = Objective.Objective(compute_potential, Uu, p, precondStrategy)
+    objective = ConstrainedObjective.ConstrainedObjective(compute_potential, compute_constraints, Uu, p, lam0, kappa0)
 
     write_output(create_field(Uu, p), p, step=0)
-        
+    
     steps = 20
-    maxDisp = 2*1.52e-3
+    maxDisp = 3*1.52e-3
     for i in range(1, steps):
         print('--------------------------------------')
         print('LOAD STEP ', i)
 
         disp -= maxDisp/steps
         p = Objective.param_index_update(p, 0, disp)
-        Uu = EquationSolver.nonlinear_equation_solve(objective,
-                                                     Uu,
-                                                     p,
-                                                     solverSettings)
+        contactDists = compute_constraints(Uu, p)
+        print(f"Contact dists = {np.amin(contactDists)}, {np.amax(contactDists)}")
+        # Uu = EquationSolver.nonlinear_equation_solve(objective,
+        #                                              Uu,
+        #                                              p,
+        #                                              solverSettings)
+        Uu = AlSolver.augmented_lagrange_solve(objective, Uu, p, alSettings, solverSettings)
         U = create_field(Uu, p)
         write_output(U, p, i)
 
