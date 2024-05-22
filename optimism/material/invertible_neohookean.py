8666b97c3f48d4eb1be2ad4c33b52433a24e4264
Brandon Talamini <talamini1@llnl.gov>
2024-05-22 11:05:17 -0700

    Make J extrapolation point a material parameter instead of hard coded

--- optimism/material/invertible_neohookean.py
+++ optimism/material/invertible_neohookean.py
@@ -5,15 +5,15 @@ from optimism import ScalarRootFind
 from optimism import TensorMath
 from optimism.material.MaterialModel import MaterialModel
 
-CONTINUATION_POINT = 0.9
 
 def create_material_model_functions(properties):
     lam, mu = _make_properties(properties['elastic modulus'],
                                properties['poisson ratio'])
+    J_min = properties['J extrapolation point']
             
     def strain_energy(dudX, internal_variables, dt):
         del internal_variables, dt
-        return energy_density(dudX, lam, mu)
+        return energy_density(dudX, lam, mu, J_min)
 
     def compute_state_new(dudX, internal_variables, dt):
         del dudX, dt
@@ -31,9 +31,9 @@ def _make_properties(E, nu):
     mu = 0.5*E/(1 + nu)
     return lam, mu
 
-def energy_density(dudX, lam, mu):
+def energy_density(dudX, lam, mu, J_min):
     J = TensorMath.det(dudX + np.identity(3))
-    return np.where(J >= CONTINUATION_POINT, standard_energy(dudX, lam, mu), continued_energy(dudX, lam, mu))
+    return np.where(J >= J_min, standard_energy(dudX, lam, mu), continued_energy(dudX, lam, mu, J_min))
 
 def standard_energy(dudX, lam, mu):
     I1m3 = 2*np.trace(dudX) + np.tensordot(dudX, dudX)
@@ -41,7 +41,7 @@ def standard_energy(dudX, lam, mu):
     logJ = np.log1p(Jm1)
     return 0.5*mu*I1m3 - mu*logJ + 0.5*lam*logJ**2
 
-def continued_energy(dudX, lam, mu):
+def continued_energy(dudX, lam, mu, J_min):
     F = dudX + np.identity(3)
     J = np.linalg.det(F)
     C = F.T@F
@@ -54,8 +54,7 @@ def continued_energy(dudX, lam, mu):
     I2 = ee[0]*ee[1] + ee[1]*ee[2] + ee[2]*ee[0]
     I3 = ee[0]*ee[1]*ee[2]
     solver_settings = ScalarRootFind.get_settings(x_tol=1e-8)
-    Jmin = 1 - CONTINUATION_POINT
-    s, _ = ScalarRootFind.find_root(lambda x: I3*x**3 + I2*x**2 + I1*x + Jmin, 0.5, np.array([0.0, 1.0]), solver_settings)
+    s, _ = ScalarRootFind.find_root(lambda x: I3*x**3 + I2*x**2 + I1*x + (1 - J_min), 0.5, np.array([0.0, 1.0]), solver_settings)
     q = r + s*ee
     h = np.linalg.norm(stretches - q)
     v = h*ee/np.linalg.norm(ee) # h*u in the paper
@@ -63,7 +62,6 @@ def continued_energy(dudX, lam, mu):
     psi0, psi1 = jax.jvp(_energy_from_principal_stretches, (q, lam, mu), (v, 0.0, 0.0))
     _, Av = jax.jvp(jax.grad(_energy_from_principal_stretches, 0), (q, lam, mu), (v, 0.0, 0.0))
     psi2 =  0.5*np.dot(v, Av)
-    # jax.debug.print("psi2={psi2}", psi2=psi2)
     return psi0 + psi1 + psi2
 
 def _energy_from_principal_stretches(stretches, lam, mu):
@@ -72,34 +70,3 @@ def _energy_from_principal_stretches(stretches, lam, mu):
 
 def _make_initial_state():
     return np.array([])
-
-
-def cubic_one_real_root(b):
-    a1 = b[2]/b[3]
-    a2 = b[1]/b[3]
-    a3 = b[0]/b[3]
-    Q = (a1**2 - 3*a2)/9
-    R = (2*a1**3 - 9*a1*a2 + 27*a3)/54
-    A = -np.sign(R)*(np.sqrt(R**2 - Q**3) + np.abs(R))**(1/3)
-    B = np.where(A != 0, Q/A, 0.0)
-    return A + B - a1/3
-
-def cubic_three_real_roots(b):
-    jax.debug.print("b={b}", b=b)
-    a1 = b[2]/b[3]
-    a2 = b[1]/b[3]
-    a3 = b[0]/b[3]
-    Q = (a1**2 - 3*a2)/9
-    R = (2*a1**3 - 9*a1*a2 + 27*a3)/54
-    theta = np.acos(R/np.sqrt(Q**3))
-    x = -2*np.sqrt(Q)*np.cos(np.array([theta, theta + 2*np.pi, theta - 2*np.pi])/3) - a1/3
-    jax.debug.print("Q={Q}, R={R}, theta={theta}, x={x}", Q=Q, R=R, theta=theta, x=x)
-    return np.sort(x)[-1]
-
-def solve_cubic(b):
-    a1 = b[2]/b[3]
-    a2 = b[1]/b[3]
-    a3 = b[0]/b[3]
-    Q = (a1**2 - 3*a2)/9
-    R = (2*a1**3 - 9*a1*a2 + 27*a3)/54
-    np.where()
